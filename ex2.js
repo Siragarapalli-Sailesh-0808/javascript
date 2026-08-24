f4659a417a4812c49f7699050a55f66a2e6ded6b
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-24 14:41:53 +0530

    Done some excerises

--- ex2.js
+++ ex2.js
@@ -34,3 +34,58 @@ function updateAAddress(street,city,zipCode) {
 
 const newAddress = new updateAAddress("TEst Adress","Anytown","12345");
 console.log(newAddress); 
+
+function areSame(address1,address2){
+    return address1.street === address2.street &&
+        address1.city === address2.city &&
+      
+        address1.zipCode === address2.zipCode;
+}
+
+console.log(areSame(address, newAddress));
+
+
+
+
+let post = {
+    title: 'a',
+    body : 'b',
+    author : 'c',
+    views : 10,
+    comments: [
+        { author: 'a', body: 'b' },
+        { author: 'c', body: 'd' }
+    ],
+    isLive : true
+}
+console.log(post);
+console.log(post.title);
+console.log(post.body);
+console.log(post.author);
+console.log(post.views);
+console.log(post.comments);
+console.log(post.isLive);
+
+// Constructor Function
+function Post(title, body, author,views,comments,isLive) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = views;
+    this.comments = comments;
+    this.isLive = isLive;
+}
+
+console.log(new Post('a', 'b', 'c', 10, [{ author: 'a', body: 'b' }, { author: 'c', body: 'd' }], true));
+
+
+let priceRanges = [
+    { label : '$' , min: 0 , max: 199},
+    { label : '$$' , min: 200 , max: 499},
+    { label : '$$$' , min: 500 , max: 1999},
+    { label : '$$$$' , min: 2000 , max: 10000}
+];
+
+let resturants = [
+   {averagePerPerson: 5}
+]
