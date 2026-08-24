292bd166549e631520614504b61416f42d4dc811
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-24 11:39:26 +0530

    Leant factory functiones

--- objects.js
+++ objects.js
@@ -7,7 +7,27 @@ let person = {
     pisApproved: false
 };
 person.name = 'vijay';
-console.log(person);
 console.log(person.name);
 
 
+const circle = {
+    radius: 1,
+    isVisible: true,
+    draw: function() {
+    console.log('draw');
+    }
+};
+circle.draw(3);
+
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+        console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
