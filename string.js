7652c7938da265822cc1988206fa7f46cd984b3d
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-24 13:36:44 +0530

    learn String literals

--- string.js
+++ string.js
@@ -7,3 +7,16 @@ console.log(message.includes('test'));
 console.log(message.startsWith('This'));
 console.log(message.endsWith('message'));
 
+
+const another = '    This is a test message      ';
+console.log(another.trim());
+
+
+//template literals
+let name = 'sailesh';
+let nameMessage = `hi ${name}`
+console.log(nameMessage);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.length);
