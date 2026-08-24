4e72a71ab10e711c040bec650c5d5def47173860
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-24 13:04:46 +0530

    learn methods like replace length includes starts with in the strings

---
+++ string.js
@@ -0,0 +1,9 @@
+const message = 'This is a test message';
+
+
+console.log(message.replace('test', 'best'));
+console.log(message.length);
+console.log(message.includes('test'));
+console.log(message.startsWith('This'));
+console.log(message.endsWith('message'));
+
