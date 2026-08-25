9fad0503a4c4cc96fb71d147c9bb7788ae79bd6d
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-25 12:32:10 +0530

    seen this keyword as this ref to the objects and arrays in javascript

--- function.js
+++ function.js
@@ -21,10 +21,60 @@ console.log(sum(1,10,4,10,5));
 
 function cart (discount,...prices) {
   const total = prices.reduce((a,b) => {return a + b});
-  return total * (discount);
+  return total * (1 - discount);
 }
 
-console.log(cart(0.1,20,30,40));
+console.log(cart(0.1,20,30,40)); 
+
+// getter and setter
+// Try and Catch
+const person = {
+    firstName : 'Sailesh',
+    lastName : 'Siragarapalli',
+    set fullName(value) {
+        if (typeof value !== "string")
+            throw new Error("Name should be a string");
+        const parts = value.split(' ');
+        if (parts.length !== 2) {
+            throw new Error("Enter a valid full name");
+        }
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+};
+
+try {
+    person.fullName = 'Vijay Sailesh';
+}
+catch (error) {
+    alert(error);
+}
+
+person.fullName = 'Karri Vijay';
+console.log(person);
+
+
+// This KeyWord
+
+const video = {
+    title : 'a',
+    tags : ['a','b','c'],
+    showTags(){
+        this.tags.forEach(function(tag){
+            console.log(this.title, tag);
+        },this);
+    }
+};
+
+video.showTags();
+
+
+
+
+
+
+
+
   
 
 
