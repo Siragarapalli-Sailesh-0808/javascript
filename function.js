e49b54dcd7d17074de9b3dbd58ef0cd8bfb283ca
Sailesh Siragarapalli <saileshsiragarapalli@heybyte.in>
2026-08-25 06:52:48 +0530

    here learnedabout arguments function where js has built it argument function where it can pass all the aruguments with out the the parameters

--- function.js
+++ function.js
@@ -1,6 +1,22 @@
+// hoisting
+
 walk();
 function walk () {
     console.log("walk");
 }
+
+
+// buit in arguments function
+
+function sum(){
+    let total = 0;
+    for (let value of arguments)
+        total += value;
+    return total;
+}
+
+console.log(sum(1,10,4,10,5));
+  
+
 
 
